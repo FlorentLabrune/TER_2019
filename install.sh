d375530ce8a20f31c86f862d672360436cefe502
Firerazzer <munster31700@gmail.com>
2020-03-19 17:50:56 +0100

    install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+DIR_MODBUS="/usr/local/include/modbus/modbus.h"
+DIR_COURANT=$(pwd)
+if [ ! -f $DIR_MODBUS ]; then
+	echo "libmodbus have to be intalled, do you want to install it ? [y/n] :"
+	read -p "" rep
+	if [ "$rep" != "y" ]; then
+		exit
+	elif ! [ -x "$(command -v autoconf)" ]; then
+		echo -e "you need autoconf, please run : \n\t sudo apt-get install autoconf"
+		exit
+	else
+		if [ "$EUID" -ne 0 ]
+  			then echo "Please run as root"
+  			exit
+		fi
+		git clone https://github.com/stephane/libmodbus.git /tmp/modbus
+		cd /tmp/modbus
+		./autogen.sh
+		./configure
+		sudo make install
+	fi
+fi
+
+cd $DIR_COURANT/MONTRAC/catkin_ws/
+catkin_make --force
+catkin_make install
