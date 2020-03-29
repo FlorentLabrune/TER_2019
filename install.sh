3e31c7130e6cca0af68e589819f8203bc1b19c02
Firerazzer <munster31700@gmail.com>
2020-03-29 20:07:44 +0200

    install ok

--- install.sh
+++ install.sh
@@ -4,23 +4,8 @@ source /opt/ros/kinetic/setup.bash
 DIR_MODBUS="/usr/local/include/modbus/modbus.h"
 DIR_COURANT=$(pwd)
 if [ ! -f $DIR_MODBUS ]; then
-	echo "libmodbus have to be intalled, do you want to install it ? [y/n] :"
-	read -p "" rep
-	if [ "$rep" != "y" ]; then
+		echo -e "libmodbus have to be intalled, please run : \n\t sudo install_modbus.sh"
 		exit
-	elif ! [ -x "$(command -v autoconf)" ]; then
-		echo -e "you need autoconf, please run : \n\t sudo apt-get install autoconf"
-		exit
-	else
-		if [ "$EUID" -ne 0 ]
-  			then echo "Please run as root"
-  			exit
-		fi
-		git clone https://github.com/stephane/libmodbus.git /tmp/modbus
-		cd /tmp/modbus
-		./autogen.sh
-		./configure
-		sudo make install
 	fi
 fi
 
