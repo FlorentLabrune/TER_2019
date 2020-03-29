c46c40a0fed3d1f79f3db9428af1ee0466c5992b
Firerazzer <munster31700@gmail.com>
2020-03-29 20:09:24 +0200

    install ok

--- install.sh
+++ install.sh
@@ -4,12 +4,7 @@ source /opt/ros/kinetic/setup.bash
 DIR_MODBUS="/usr/local/include/modbus/modbus.h"
 DIR_COURANT=$(pwd)
 if [ ! -f $DIR_MODBUS ]; then
-	echo "libmodbus have to be intalled, do you want to install it ? [y/n] :"
-	read -p "" rep
-	if [ "$rep" != "y" ]; then
-		exit
-	else
-		echo -e "you need modbuslib, please run : \n\t sudo install_modbus.sh"
+		echo -e "libmodbus have to be intalled, please run : \n\t sudo install_modbus.sh"
 		exit
 	fi
 fi
