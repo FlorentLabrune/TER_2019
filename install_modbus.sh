9e35212bba398752fdd25cf4be62ffc0a0018ba2
Firerazzer <munster31700@gmail.com>
2020-03-29 20:09:44 +0200

    installmodbus ok

--- install_modbus.sh
+++ install_modbus.sh
@@ -5,6 +5,11 @@ if [ "$EUID" -ne 0 ]
   			exit
 fi
 
+echo "libmodbus and autoconf will be intalled, do you want to install ? [y/n] :"
+read -p "" rep
+if [ "$rep" != "y" ]; then
+	exit
+
 if ! [ -x "$(command -v autoconf)" ]; then
 	sudo apt-get install autoconf
 fi
