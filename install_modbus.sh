00eeb8796c27f425fec1b514d828a5ace97431a4
Firerazzer <munster31700@gmail.com>
2020-03-29 20:02:15 +0200

    modbus + autoconf

--- install_modbus.sh
+++ install_modbus.sh
@@ -3,9 +3,14 @@
 if [ "$EUID" -ne 0 ]
   			then echo "Please run as root"
   			exit
-		fi
-		git clone https://github.com/stephane/libmodbus.git /tmp/modbus
-		cd /tmp/modbus
-		./autogen.sh
-		./configure
-		sudo make install
+fi
+
+if ! [ -x "$(command -v autoconf)" ]; then
+	sudo apt-get install autoconf
+fi
+
+git clone https://github.com/stephane/libmodbus.git /tmp/modbus
+cd /tmp/modbus
+./autogen.sh
+./configure
+sudo make install
